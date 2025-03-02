cff604aafbe3a7ee3c2b3ad14f325051681840f8
Rohit Gupta <rjgupta2116@gmail.com>
2025-03-02 11:58:38 +0530

    chore: applying lazy loading on image

--- src/components/UI/Ring.jsx
+++ src/components/UI/Ring.jsx
@@ -1,14 +1,20 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 export default function Ring() {
-    return (
-        <div className="container-loader">
-            <div className="round"></div>
-            <div className="round"></div>
-            <div className="image-container">
-                <Image src="/Assets/Images/me.jpg" width={1000} height={1000} alt="Image" />
-            </div>
-        </div>
-    )
+  return (
+    <div className="container-loader">
+      <div className="round"></div>
+      <div className="round"></div>
+      <div className="image-container">
+        <Image
+          src="/Assets/Images/me.jpg"
+          width={1000}
+          height={1000}
+          alt="Image"
+          loading="lazy"
+        />
+      </div>
+    </div>
+  );
 }
